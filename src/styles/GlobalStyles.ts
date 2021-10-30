34fecf9624ac58c66e4a73d8a41946c86631dd03
Vitor Windberg de Lima <vitorwlima13@gmail.com>
2021-10-29 23:19:45 -0300

    add initial setup and global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+import { colors } from './colors'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 10px;
+  }
+
+  body {
+    font-family: Roboto, sans-serif;
+    font-size: 1.6rem;
+    height: 100vh;
+    color: ${colors.black};
+    background-color: ${colors.primary};
+  }
+`
---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+const colors = {
+  primary: '#76DFCC',
+  secondary: '#CD471C',
+  black: '#1D1F1F',
+  white: '#EFF6F5',
+}
+
+export { colors }
